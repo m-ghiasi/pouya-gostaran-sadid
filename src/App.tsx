96101b92fd9dec62244f8ce6c3c1f3a3becc7e0c
m-ghiasi <mahdiye71gh@gmail.com>
2025-02-03 14:58:17 +0330

    edite routin

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import License from "./pages/License";
 import CheshmAndaz from "./pages/CheshmAndaz";
 import Dispaching from "./pages/Dispaching";
 import Ability from "./pages/Ability";
-import Kahab from "./pages/kahab";
+import Kahab from "./pages/Kahab";
 
 function App() {
   return (
@@ -22,14 +22,21 @@ function App() {
           <Route index element={<HomePage />}></Route>
           <Route path="HomePage" element={<HomePage />}></Route>
 
-            
-          <Route  path="HomePage/paper" element={<Paper />}></Route>
-          <Route  path="HomePage/License" element={<License />}></Route>
-          <Route  path="HomePage/CheshmAndaz" element={< CheshmAndaz/>}></Route>
-          <Route  path="HomePage/Dispaching" element={<Dispaching />}></Route>
-          <Route  path="HomePage/kahab" element={<Kahab />}></Route>
-          <Route  path="HomePage/Ability" element={<Ability />}></Route>
-          
+          <Route path="HomePage/paper" element={<Paper />}></Route>
+          <Route path="HomePage/License" element={<License />}></Route>
+          <Route path="HomePage/CheshmAndaz" element={<CheshmAndaz />}></Route>
+
+          <Route path="HomePage/Dispaching" element={<Dispaching />}></Route>
+          <Route path="HomePage/kahab" element={<Kahab />}></Route>
+          <Route path="HomePage/Ability" element={<Ability />}></Route>
+
+          <Route path="paper" element={<Paper />}></Route>
+          <Route path="License" element={<License />}></Route>
+          <Route path="CheshmAndaz" element={<CheshmAndaz />}></Route>
+
+          <Route path="Dispaching" element={<Dispaching />}></Route>
+          <Route path="kahab" element={<Kahab />}></Route>
+          <Route path="Ability" element={<Ability />}></Route>
 
           <Route path="Managers" element={<Managers />}></Route>
           <Route path="Services" element={<Services />}></Route>
